de2a53fb83ecc4f45024a1c6fb8f31429ad24ed1
Wayne Miller <wayne.miller@vecna.com>
2016-10-19 09:27:48 -0400

    DDV-7: Redoing copyright notices.
    Updated copyright notices of Java files to be more compliant with GPL.
    Also added AUTHORS file.

--- src/bscmail/Shift.java
+++ src/bscmail/Shift.java
@@ -1,8 +1,22 @@
 /*
- * This code is protected by copyright and licensed under the GNU
- * General Public License version 3.  For details, see the file
- * "LICENSE" or visit https://www.gnu.org/licenses/gpl-3.0.en.html.
+ * Copyright © 2014-2016 its authors.  See the file "AUTHORS" for details.
+ *
+ * This file is part of BSCMail.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package bscmail;
 
 import java.io.Serializable;
